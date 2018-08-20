5608c62f9d8f424a214755d0b7a50f689335e89e
wangxiao <abcdefgh_5142@qq.com>
2018-08-20 15:56:43 +0800

    1

--- app/controller/home.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const Controller = require('egg').Controller;
-class HomeController extends Controller {
-  async index() {
-    this.ctx.body = 'hi, egg ' + process.pid + '\n';
-  }
-}
-
-module.exports = HomeController;
